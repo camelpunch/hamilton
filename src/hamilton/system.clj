265751737e43770a33665c17d51644e65b854b14
Andrew Bruce <andrew@camelpunch.com>
2015-01-23 01:45:49 +0000

    Only stop running web servers

--- src/hamilton/system.clj
+++ src/hamilton/system.clj
@@ -14,7 +14,7 @@
 
   (stop [web-server]
     (println "Stopping Jetty")
-    (.stop running-server)
+    (when (:running-server web-server) (.stop (:running-server web-server)))
     (assoc web-server :running-server nil)))
 
 (defn new-web-server [router port]
